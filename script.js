2ce2c7185a48a6d799763199f79b00f1f279b1ad
Moa Schaffer <moa.schaffer@gmail.com>
2021-11-17 14:54:06 +0100

    Adjusted main content to mobile menu

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".dropdown-content");
+const mainFlexbox = document.querySelector(".main-flexbox")
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("show");
+    mainFlexbox.classList.toggle("adaptToMenu");
 }
