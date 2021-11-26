2a2e4eccd8d4a5bfb78d0fe4cfff763f153b5710
Sebaba95 <93581326+Sebaba95@users.noreply.github.com>
2021-11-26 16:49:08 +0100

    Update script.js
    Added comments.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Get elements class = "hamburger", class = "dropdown-content" and class = "main-flexbox".
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".dropdown-content");
 const mainFlexbox = document.querySelector(".main-flexbox")
 
+// Add an eventlistner to the element hamburger which will activate function mobileMenu with action click.
 hamburger.addEventListener("click", mobileMenu);
 
+// Function mobileMenu
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("show");
-    mainFlexbox.classList.toggle("adaptToMenu");
+    hamburger.classList.toggle("active"); // Toggle between showing the hamburger menu and a cross.
+    navMenu.classList.toggle("show"); // Toggle between showing and not showing the mobile menu.
+    mainFlexbox.classList.toggle("adaptToMenu"); // Toggle between adapting the main flexbox to the header and the navMenu..
 }
